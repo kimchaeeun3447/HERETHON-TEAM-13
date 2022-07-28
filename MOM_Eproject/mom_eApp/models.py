b7f445486111d78cfcc513e3ff766e447eed935a
kimchaeeun3447 <rlacodms3447@naver.com>
2022-07-29 06:53:51 +0900

    [MOD] 참조 필드명 변경 #5

--- MOM_Eproject/mom_eApp/models.py
+++ MOM_Eproject/mom_eApp/models.py
@@ -12,7 +12,7 @@ class User(AbstractUser):
     level = models.CharField(max_length=10)
 
 class Class(models.Model):
-    image = models.ImageField(upload_to=None, blank=False)
+    image = models.ImageField(upload_to='classImages', blank=False)
     title = models.CharField(max_length=50)
     category = models.CharField(max_length=10, default=NULL, null=True)
     start_time = models.DateTimeField()
@@ -31,8 +31,8 @@ class Class(models.Model):
 
 
 class Review(models.Model):
-    user_id = models.ForeignKey(User, on_delete = models.CASCADE, blank=True)
-    class_id = models.ForeignKey(Class, on_delete = models.CASCADE, blank=True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, blank=True)
+    applyClass = models.ForeignKey(Class, on_delete = models.CASCADE, blank=True)
     score = models.IntegerField()
     content = models.TextField()
     time = models.DateTimeField(default=timezone.now)
@@ -41,16 +41,16 @@ class Review(models.Model):
         ordering = ['-time'] #최신순 정렬
 
     def __str__(self):
-        return f"작성자:{self.user_id} - 클래스:{self.class_id}"
+        return f"작성자:{self.user} - 클래스:{self.applyClass}"
 
 
 class Apply(models.Model):
-    user_id = models.ForeignKey(User,on_delete = models.CASCADE, blank=True)
-    class_id = models.ForeignKey(Class, on_delete = models.CASCADE, blank=True)  
+    user = models.ForeignKey(User,on_delete = models.CASCADE, blank=True)
+    applyClass = models.ForeignKey(Class, on_delete = models.CASCADE, blank=True)  
     time = models.DateTimeField(default=timezone.now)
     
     class Meta:
         ordering = ['-time'] #최신순 정렬
 
     def __str__(self):
-        return f"지원자:{self.user_id} - 클래스:{self.class_id}"
+        return f"지원자:{self.user} - 클래스:{self.applyClass}"
