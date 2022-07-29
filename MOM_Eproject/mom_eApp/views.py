875954e51c57c62c000532b8ad0f80fa7cb3ced0
Tae Young <k55416325@gamil.com>
2022-07-30 03:56:44 +0900

    FEAT : date formatting 완료, 클래스 신청여부 진행 중

--- MOM_Eproject/mom_eApp/views.py
+++ MOM_Eproject/mom_eApp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 
 #모델 호출 : 태영
 from .models import Class
+from .models import User
+from .models import Apply
 
 # 메인 화면
 def home(request):
@@ -10,4 +12,11 @@ def home(request):
 # 클래스 디테일 화면 : 태영
 def detail(request, lesson_id):
     lesson = get_object_or_404(Class, pk=lesson_id)
-    return render(request, 'detail.html', {'lesson':lesson})
+    apply = get_object_or_404(Apply)
+    return render(request, 'detail.html', {'lesson':lesson, 'apply':apply})
+
+
+# 클래스 등록 화면 : 태영
+def apply(request, lesson_id):
+    lesson = get_object_or_404(Class, pk=lesson_id)
+    return render(request, 'apply.html', {'lesson':lesson})
