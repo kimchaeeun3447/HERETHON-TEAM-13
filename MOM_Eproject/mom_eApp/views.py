a7198e38f53ff107bc980406b94485fd7f96ab50
kimchaeeun3447 <rlacodms3447@naver.com>
2022-07-30 03:07:22 +0900

    [FEAT] 홈 기능 완성 #3
    - 카테고리 필터링
    - 배너, 클래스 뷰 링크 연결
    - 로그인 유무에 따른 html 코드

--- MOM_Eproject/mom_eApp/views.py
+++ MOM_Eproject/mom_eApp/views.py
@@ -6,6 +6,7 @@ from .models import Class, Apply
 def home(request):
     # /?category=마케팅
     category = request.GET.get('category', None) #쿼리스트링 없어도 됨
+    print(category)
 
     # 상단 클래스 목록 -  로그인o -참여 예정 클래스 / 로그인x - 추천 클래스
     if request.user.is_authenticated:
@@ -26,7 +27,7 @@ def home(request):
     else:
         home_class_list = Class.objects.filter(category=category)
     
-    return render(request, 'home.html', {'user_class_list': user_class_list, 'home_class_list': home_class_list})
+    return render(request, 'home.html', {'user_class_list': user_class_list, 'home_class_list': home_class_list, 'category': category})
 
 # 클래스 디테일 화면 : 태영
 def detail(request, lesson_id):
