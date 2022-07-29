01d235b696b6ab74ca6f0703597dd13af812c13c
Tae Young <k55416325@gamil.com>
2022-07-29 20:46:11 +0900

    FEAT : detail view 기본 기능 구현(date formatting전)

--- MOM_Eproject/MOM_Eproject/urls.py
+++ MOM_Eproject/MOM_Eproject/urls.py
@@ -22,6 +22,7 @@ from mom_eApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('detail/<str:lesson_id>', views.detail, name="detail") #상세페이지[GET] : 태영
 ]
 
 if settings.DEBUG:
